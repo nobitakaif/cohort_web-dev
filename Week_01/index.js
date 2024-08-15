761e0fd6d34cd1c7ccd3569ec662a1d1b2ad97f8
Mohd kaif <124164882+nobitakaif@users.noreply.github.com>
2024-08-15 07:19:57 +0000

    reading the file synchronously and asynchronously

--- Week_01/index.js
+++ Week_01/index.js
@@ -1,3 +1,10 @@
 const fs = require("fs")
-const newFile=fs.readFileSync("a.txt","utf-8");
-console.log(newFile)
+// const newFile=fs.readFileSync("a.txt","utf-8");
+// console.log(newFile)
+fs.readFile("a.txt","utf-8",(err,res)=>{
+    if(err){
+        console.log("here's the some error")
+    }else{
+        console.log(res)
+    }
+})
