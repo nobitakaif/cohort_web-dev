6d7662ec105745b0169aa0fa6436d0a738bd81b6
Mohd kaif <124164882+nobitakaif@users.noreply.github.com>
2024-08-15 11:26:20 +0000

    basic of classes and object

---
+++ Week_01/classes.js
@@ -0,0 +1,30 @@
+class test{
+    
+   constructor(a,b){
+        this.a=a
+        this.b=b
+   }
+    
+    sum(a,b){
+        return a*b
+    }
+}
+const testing= new test()
+// console.log(testing)
+console.log(testing.sum(5,5))
+
+
+
+
+
+
+
+
+// const obj={
+//     a:5,
+//     b:5,
+//     sum: function(a,b){
+//        return  a*b
+//     }
+// }
+// console.log(obj.sum(5,2))
