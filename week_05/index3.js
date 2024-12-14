d98b1c694a7811d5c6d7753ec379b0bffd2448c3
mohd kaif <mk2818356@gmail.com>
2024-12-14 14:53:50 +0530

    adding .gitignore file

--- week_05/index3.js
+++ week_05/index3.js
@@ -7,7 +7,7 @@ app.post('/sum',function(req,res){
     const a = parseInt(req.body.a)
     const b = parseInt(req.body.b)
     console.log(a+b)
-    res.send('your output is ' + a+b)
+    res.send('your output is ' + parseInt(a+b))
 })
 app.listen(5000,()=>{
     console.log('server is running on port 5000')
